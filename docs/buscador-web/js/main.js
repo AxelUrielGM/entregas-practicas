437062d21225588a59e9d09cdf6c7e7b9b3f8ae0
Axel Gutierrez <cdmx2972@amerike.edu.mx>
2025-03-29 06:41:35 -0600

    Avance Buscador Web 1.3

--- docs/buscador-web/js/main.js
+++ docs/buscador-web/js/main.js
@@ -2,14 +2,13 @@ const CienciasSociales = [
     { name: "Administración Pública", url: "LXO/CienciasSociales/administracion-publica.html" },
     { name: "Criminología", url: "LXO/CienciasSociales/criminologia.html" },
     { name: "Derecho", url: "LXO/CienciasSociales/derecho.html" },
+    { name: "Licenciatura Ejecutiva en Educación", url: "LXO/CienciasSociales/educacion.html" },
     { name: "Historia", url: "LXO/CienciasSociales/historia.html" },
     { name: "Lengua y Literatura", url: "LXO/CienciasSociales/lengua-literatura.html" },
     { name: "Lenguas Extranjeras", url: "LXO/CienciasSociales/lenguas-extranjeras.html" },
-    { name: "Licenciatura Ejecutiva en Ciencias de la Comunicación Online", url: "LXO/CienciasSociales/ciencias-comunicacion.html" },
-    { name: "Licenciatura Ejecutiva en Educación Online", url: "LXO/CienciasSociales/educacion.html" },
     { name: "Licenciatura Ejecutiva en Pedagogía", url: "LXO/CienciasSociales/pedagogia.html" },
-    { name: "Licenciatura Ejecutiva en Relaciones Públicas", url: "LXO/CienciasSociales/relaciones-publicas.html" },
     { name: "Relaciones Internacionales", url: "LXO/CienciasSociales/relaciones-internacionales.html" },
+    { name: "Licenciatura Ejecutiva en Relaciones Públicas", url: "LXO/CienciasSociales/relaciones-publicas.html" },
     { name: "Seguridad Pública", url: "LXO/CienciasSociales/seguridad-publica.html" },
     { name: "Sociología", url: "LXO/CienciasSociales/sociologia.html" }
 ];
@@ -27,30 +26,31 @@ const HospitalidadTurismoGastronomia = [
 ];
 
 const Ingenierias = [
-    { name: "Desarrollo de Videojuegos", url: "LXO/Ingenierias/desarrollo-videojuegos.html" },
-    { name: "Ing. en Ciencia de Datos Online", url: "LXO/Ingenierias/ciencia-datos.html" },
-    { name: "Ing. en Diseño de Software y Redes", url: "LXO/Ingenierias/diseno-software-redes.html" },
     { name: "Ing. en Electrónica", url: "LXO/Ingenierias/electronica.html" },
+    { name: "Ing. en Ciencia de Datos", url: "LXO/Ingenierias/ciencia-datos.html" },
+    { name: "Desarrollo de Videojuegos", url: "LXO/Ingenierias/desarrollo-videojuegos.html" },
+    { name: "Ing. en Diseño de Software y Redes", url: "LXO/Ingenierias/diseno-software-redes.html" },
     { name: "Ing. en Física", url: "LXO/Ingenierias/fisica.html" },
+    { name: "Ingeniería en Logistica", url: "LXO/Ingenierias/logistica.html" },
     { name: "Ing. en Matemáticas", url: "LXO/Ingenierias/matematicas.html" },
     { name: "Ing. en Mecatrónica", url: "LXO/Ingenierias/mecatronica.html" },
     { name: "Ing. en Negocios y Manufactura", url: "LXO/Ingenierias/negocios-manufactura.html" },
     { name: "Ing. en Tecnologías y Sistemas de la Información", url: "LXO/Ingenierias/tecnologias-sistemas.html" },
-    { name: "Ingeniería en Logística Online", url: "LXO/Ingenierias/logistica.html" },
-    { name: "Ingeniería Industrial y de Sistemas Online", url: "LXO/Ingenierias/industrial-sistemas.html" }
+    { name: "Ingeniería Industrial y de Sistemas", url: "LXO/Ingenierias/industrial-sistemas.html" }
 ];
 
 const Negocios = [
     { name: "Actuaría", url: "LXO/Negocios/actuaria.html" },
+    { name: "Administración de Empresas", url: "LXO/Negocios/administracion-empresas.html" },
     { name: "Administración de Empresas del Entretenimiento", url: "LXO/Negocios/administracion-entretenimiento.html" },
     { name: "Administración de Negocios Internacionales", url: "LXO/Negocios/negocios-internacionales.html" },
     { name: "Administración de Recursos Humanos", url: "LXO/Negocios/recursos-humanos.html" },
     { name: "Comercio y Logística Internacionales", url: "LXO/Negocios/comercio-logistica.html" },
+    { name: "Comunicación y Medios Digitales", url: "LXO/Negocios/comunicacion-medios-digitales.html" },
     { name: "Contaduría Pública y Finanzas", url: "LXO/Negocios/contaduria-finanzas.html" },
     { name: "Economía y Finanzas", url: "LXO/Negocios/economia-finanzas.html" },
     { name: "Estrategia y Gestión de Ventas", url: "LXO/Negocios/gestion-ventas.html" },
     { name: "Finanzas y Banca", url: "LXO/Negocios/finanzas-banca.html" },
-    { name: "Licenciatura Ejecutiva en Administración de Empresas Online", url: "LXO/Negocios/administracion-empresas.html" },
     { name: "Mercadotecnia", url: "LXO/Negocios/mercadotecnia.html" },
     { name: "Mercadotecnia y Publicidad en Entornos Digitales", url: "LXO/Negocios/mercadotecnia-digital.html" }
 ];
